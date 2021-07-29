b24301b5f2a84e2e88781a7a411b3483e52de683
Oytizzo <oytizzo@gmail.com>
2021-07-29 16:24:20 +0600

    Add one button infinite click

--- js-basics/index.js
+++ js-basics/index.js
@@ -4,5 +4,12 @@ console.log("bla bla bla")
 // alert("hey")
 // document.getElementById("demo").innerHTML = "Hello JavaScript!"
 function changeText(){
-    document.getElementById("demo").innerHTML = "Hello JS";
+    let element = document.getElementById("demo");
+    
+    if (element.innerHTML == "Hello") {
+        element.innerHTML = "bye";
+    }
+    else {
+        element.innerHTML = "Hello"
+    }
 }
